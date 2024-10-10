deb1a69de57e31b42aa0a3cdbfedc1417bb74fe6
divyashyam194 <divyaganpathy54@gmail.com>
2024-10-10 11:11:29 +0530

    Doctor_rejection & register_patient

--- hospitalsystem/hospital/admin.py
+++ hospitalsystem/hospital/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Doctor , Patient
+from .models import Doctor , Patient,RejectedDoctors
 
 
-admin.site.register([Doctor,Patient])
+admin.site.register([Doctor,Patient,RejectedDoctors])
